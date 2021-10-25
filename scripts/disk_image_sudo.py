adf3c9d7ba8f8cbcd0b8f984474501f1b81bcb9d
Grzegorz Krukowski <grkrukowski@gmail.com>
2021-10-25 13:12:44 +0200

    Update scripts to newest version

--- scripts/disk_image_sudo.py
+++ scripts/disk_image_sudo.py
@@ -8,7 +8,7 @@ def create_disk_image(dir_path, disk_image_path):
     logging.info("Creating disk image...")
 
     dirname = os.path.dirname(__file__)
-    script_path = os.path.join(dirname, "./create-dmg-no-sudo")
+    script_path = os.path.join(dirname, "./create-dmg-sudo")
 
     subprocess.run(
         f'{script_path} --volname "Debug" --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon "{__APP_NAME}" 200 190 --hide-extension "{__APP_NAME}" --app-drop-link 600 185 "{disk_image_path}" {dir_path}', shell=True, check=True)
